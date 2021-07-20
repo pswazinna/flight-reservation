8e1c79d08cbaf9825b45810fcc196814acdd23d1
Phillip Swazinna <p.swazinna@tum.de>
2021-07-20 14:32:37 +0200

    useless comment

--- src/main/java/flight/reservation/Passenger.java
+++ src/main/java/flight/reservation/Passenger.java
@@ -2,7 +2,7 @@ package flight.reservation;
 
 public class Passenger {
 
-    private final String name;
+    private final String name; // a useless comment
 
     public Passenger(String name) {
         this.name = name;
